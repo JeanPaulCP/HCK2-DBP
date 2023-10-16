6b36e7ccbd2a73e0b3ae53e70ca9e4b933c0b50f
Paul <jean.cuzcano@utec.edu.pe>
2023-10-16 16:35:34 -0500

    Sexto avance.

--- src/main/java/com/example/hack_2/E_1/Group.java
+++ src/main/java/com/example/hack_2/E_1/Group.java
@@ -17,4 +17,43 @@ public class Group{
 
     @OneToMany
     private List<Person> persons;
+
+    // Constructores
+
+    public Group() {} // Constructor vacío
+
+    public Group(Long id, String name, List<Person> persons) {
+        this.id = id;
+        this.name = name;
+        this.persons = persons;
+    }
+
+    // Getters
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public List<Person> getPersons() {
+        return this.persons;
+    }
+
+    // Setters
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPersons(List<Person> persons) {
+        this.persons = persons;
+    }
+
 }
--- src/main/java/com/example/hack_2/E_2/Person.java
+++ src/main/java/com/example/hack_2/E_2/Person.java
@@ -18,4 +18,42 @@ public class Person{
     @OneToMany
     private List<Group> groups;
 
+    // Constructores
+
+    public Person() {} // Constructor vacío
+
+    public Person(Long id, String name, List<Group> groups) {
+        this.id = id;
+        this.name = name;
+        this.groups = groups;
+    }
+
+    // Getters
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public List<Group> getGroups() {
+        return this.groups;
+    }
+
+    // Setters
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGroups(List<Group> groups) {
+        this.groups = groups;
+    }
+
 }
